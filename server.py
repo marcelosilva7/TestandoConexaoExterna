31208e56d6f902a1ea449ba4bbdfcd7680f9d7d7
Marcelo Silva <marcelo.silva070899@gmail.com>
2024-03-12 08:40:59 -0300

    ip acess

--- server.py
+++ server.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, Request, Response, status, HTTPException
+from starlette.middleware.base import BaseHTTPMiddleware
+
+import os
+
+allowed_ip = os.getenv("IP_PERMITIDO")
+
+class OnlyOneIPMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, allowed_ip: str):
+        super().__init__(app)
+        self.allowed_ip = allowed_ip
+
+    async def dispatch(self, request: Request, call_next):
+        client_ip = request.client.host
+        if client_ip == self.allowed_ip:
+            return await call_next(request)
+        return Response(content="Acesso negado.", status_code=status.HTTP_403_FORBIDDEN)
+
+
 
 lista = [
+
     {"cpf": "12345678911", "nome": "rodolfo", "nomemae": "maria", "nascimento": "10/10/2001"},
     {"cpf": "98765432199", "nome": "joao", "nomemae": "joaquina", "nascimento": "09/09/2002"}
+
 ]
 
 app = FastAPI()
